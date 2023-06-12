96dc5ebb7aacaf3e9eaf9e139a551ba944eff8d5
Vitaliy Vasilenko <me@vitalvas.com>
2023-06-12 12:46:48 -0400

    fix: move app to cmd

---
+++ cmd/server/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/vitalvas/proxmox-cloud-resource-scheduler/cmd/server/app"
+
+func main() {
+	app.New().Run()
+}
--- main.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "github.com/vitalvas/proxmox-cloud-resource-scheduler/app"
-
-func main() {
-	this := app.New()
-	this.Run()
-}
