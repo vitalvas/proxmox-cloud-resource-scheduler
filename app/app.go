adcbf5e34a5d454e92dca08cf343a1a97c83add8
Vitaliy Vasilenko <me@vitalvas.com>
2023-06-12 12:11:47 -0400

    feat: move config to internal

--- app/app.go
+++ app/app.go
@@ -1,16 +1,25 @@
 package app
 
-import "github.com/vitalvas/proxmox-cloud-resource-scheduler/internal/proxmox"
+import (
+	"log"
+
+	"github.com/vitalvas/proxmox-cloud-resource-scheduler/internal/config"
+	"github.com/vitalvas/proxmox-cloud-resource-scheduler/internal/proxmox"
+)
 
 type App struct {
-	config  *Config
+	config  *config.Config
 	proxmox *proxmox.Proxmox
 }
 
 func New() *App {
 	app := &App{}
 
-	app.config = LoadConfig("config.json")
+	var err error
+	app.config, err = config.LoadConfig("config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	app.proxmox = proxmox.New()
 	if app.config != nil {
